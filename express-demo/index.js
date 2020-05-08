d0a143c40c6170d109e18bb43472bff9459483c0
saurabh-singh-thakur91 <saurabh.singh.thakur91@gmail.com>
2020-05-08 16:17:20 +0530

    environment and debugging examples

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,13 +4,36 @@ const app = express();
 const logger = require('./logger');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
 
+// Use env variable like export DEBUG=app* or export DEBUG=app:startup,app:db to set debug levels
+/*
+Set the env variable while starting the application DEGUG=app:* nodemon index.js
+*/
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+
+//Configuration
+console.log('Application name: ' + config.get('name'));
+console.log('mail server: ' + config.get('mail.someprop'));
+console.log('mail password: ' + config.get('mail.password'));
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`); // process.env.NODE_ENV => undefined if not set
+console.log(`app: ${app.get('env')}`);
 
 app.use(express.json()); // middleware, use this in request processing pipeline
 app.use(express.urlencoded({ extended: true })); // parses this req and add the json to req.body
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if(app.get('env') === 'dev'){
+    app.use(morgan('tiny'));
+    startupDebugger("morgan enabled. . .")
+}
+
+// for DB debugging
+dbDebugger('Connected to the database');
+    
 
 app.use(logger);
 
