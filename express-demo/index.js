3fd111061d083af1555deeb7c1620df28ce94592
saurabh-singh-thakur91 <saurabh.singh.thakur91@gmail.com>
2020-05-08 12:32:47 +0530

    express and vidly complete

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('@hapi/joi');
 const express = require('express');
 const app = express();
 
@@ -27,6 +28,14 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+
+    const { error } = validateCourse(req.body); // equivalent to getting result.error
+
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name 
@@ -36,6 +45,61 @@ app.post('/api/courses', (req, res) => {
 
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not exists return 404
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course) // 404
+        res.status(404).send('Course with given id was not found')
+
+    // Validate the course
+    // If invalid, return 404
+
+    // const result = validateCourse(req.body);
+
+    const { error } = validateCourse(req.body); // equivalent to getting result.error
+
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    // Update course
+    // Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not exisiting return 404
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course) // 404
+        res.status(404).send('Course with given id was not found')
+
+    // delete
+
+    const index = courses.indexOf(course);
+
+    courses.splice(index, 1);
+
+    res.send(course);
+
+});
+
+function validateCourse(course){
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
